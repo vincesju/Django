53db27719e96389bc949c53e29a5865728941ff7
vincesju <justinmac9442@gmail.com>
2025-10-07 12:33:21 +0800

    Updated

--- registration/urls.py
+++ registration/urls.py
@@ -1,9 +1,7 @@
-# registration/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Full URL will be /registration/api/register/
     path('api/register/', views.register_user, name='register_user'),
+    path('api/users/', views.get_users, name='get_users'), 
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,17 +1,21 @@
-# registration/views.py
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import RegistrationSerializer # Note: Using RegistrationSerializer
+from .serializers import RegistrationSerializer
+from .models import UserRegistration 
 
 @api_view(['POST'])
 def register_user(request):
     serializer = RegistrationSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        # Return only a subset of data (exclude password, which is write-only)
         return Response(serializer.data, status.HTTP_201_CREATED) 
     
-    # Return 400 with validation errors if data is invalid
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# ADD THIS NEW FUNCTION
+@api_view(['GET'])
+def get_users(request):
+    users = UserRegistration.objects.all()
+    serializer = RegistrationSerializer(users, many=True)
+    return Response(serializer.data)
